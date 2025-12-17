037b1552a0229963e2cd25d23aa02da4f59778fb
Kai Yasko <yasko.kai@gmail.com>
2025-12-17 03:48:19 -0500

    created gui header

--- src/gui.h
+++ src/gui.h
@@ -1,4 +1,16 @@
+#include <stdint.h>
+#include "raylib.h"
+
 #ifndef GUI_H_
 #define GUI_H_
+
+typedef struct {
+	Rectangle panels[4];
+
+	uint8_t flags;
+
+} Gui;
+
+void GuiUpdate(Gui *gui);
 
 #endif
